3bf57253388a4b89ee8ecde9d7d4155df437045d
dnwthtnr <dnwthtnr@gmail.com>
2025-02-22 19:04:32 -0500

    Building core functions. Finish serialization of structs to json the nwork on setting file storage and de/serializing.

--- ticketer/core.cpp
+++ ticketer/core.cpp
@@ -1,1 +1,44 @@
 #include "core.h"
+
+Customer* create_customer(std::string name, std::string phone_number, std::string email)
+{
+	if (phone_number == "" && email == "") {
+		return nullptr;
+	};
+	Customer* cus = new Customer(name, phone_number, email);
+	return cus;
+}
+
+Note* create_note(std::string content, WorkOrder* work_order, Customer* customer)
+{
+	if (work_order == nullptr && customer == nullptr) {
+		return nullptr;
+	};
+
+	time_t t = time(NULL);
+	struct tm* date;
+	date = gmtime(&t);
+
+	std::string timestr = asctime(date);
+	Note* note = new Note(timestr, content);
+	return note;
+}
+
+WorkOrder* create_work_order(Customer* customer, std::string device, std::string description, float price)
+{
+	if (price < 0) {
+		return nullptr;
+	};
+	WorkOrder* work_order = new WorkOrder(
+		customer,
+		device,
+		description,
+		price
+	);
+	return work_order;
+}
+
+
+nlohmann::json struct_to_json(auto data) {
+	// way to agnostically serialize struct as json dict -- maybe go based off termination bits or something
+}
--- ticketer/core.h
+++ ticketer/core.h
@@ -1,4 +1,7 @@
 #include "nlohmann/json.hpp"
+#include <datetimeapi.h>
+#include <ctime>
+#include <chrono>
 
 
 
@@ -7,3 +10,31 @@ std::string write_json(nlohmann::json j, std::string filepath);
 
 
 nlohmann::json read_json(std::string filepath);
+
+
+struct Note {
+	std::string asctime;
+	std::string content;
+};
+
+struct Customer {
+	std::string name;
+	std::string number="";
+	std::string email = "";
+	std::vector<Note*> notes = {};
+};
+
+struct WorkOrder {
+	Customer* customer;
+	std::string device;
+	std::string description;
+	float price;
+	std::vector<Note*> notes = {};
+
+};
+
+Customer* create_customer(std::string name, std::string phone_number, std::string email = "");
+
+Note* create_note(std::string content, WorkOrder* work_order= nullptr, Customer* customer=nullptr);
+
+WorkOrder* create_work_order(Customer* customer, std::string device, std::string description, float price);
