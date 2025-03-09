957b9eb43520d865640e38f8dc20c41b3b311cab
dnwthtnr <dnwthtnr@gmail.com>
2025-03-09 19:12:33 -0400

    still building out functions for organizing data types. seperated ids into seperate typ.
    TODO:
    -fix bug with overloaded functions that have default parameters
    -build functions for querying struct pointer from id
    -write functions for mass serializing and then garbage collecting data structs before testing and building any ui
    -decide whether to use just qt or build a localhost for the app (likely qt just for ease )
    
    Plan:
    -ids will be serialized as a list of raw ids(unsigned ints)
    -upon deserializations first create id instances
    -when building other data types from json just populate necessary member on id struct

--- ticketer/core.cpp
+++ ticketer/core.cpp
@@ -1,15 +1,100 @@
 #include "core.h"
 
-customer::Customer* customer::create_customer(std::string name, std::string phone_number, std::string email)
+#pragma region: ID
+namespace id{
+
+	unsigned int generate_unique_id()
+	{
+		unsigned int id;
+		srand(time(0));
+
+		for (int i = 0; i < 5; i++) {
+			id << (1 + (rand() % 50));
+		};
+		std::cout << "ID Generated: " << id << std::endl;
+		return id;
+	};
+
+	id::ID* create_id(
+		unsigned int raw_id = NULL,
+		workorder::WorkOrder* work_order = nullptr,
+		customer::Customer* customer = nullptr,
+		note::Note* note = nullptr
+	)
+	{
+		id::ID* new_id;
+
+		if (raw_id == NULL) {
+			raw_id = id::generate_unique_id();
+		};
+		new_id->raw_id = raw_id;
+		new_id->customer = customer;
+		new_id->work_order = work_order;
+		new_id->note = note;
+
+		id::existing_ids.push_back(new_id);
+		return new_id;
+	};
+
+	id::ID* from_raw_id(unsigned int raw_id) {
+		for (id::ID* current_id : id::existing_ids) {
+			if (current_id->raw_id == raw_id) {
+				return current_id;
+			}
+		};
+		return NULL;
+	}
+
+	nlohmann::json existing_as_json() {
+		nlohmann::json j;
+
+
+		std::vector<unsigned int> raw_ids;
+		for (id::ID* id : id::existing_ids) {
+			raw_ids.push_back(id->raw_id);
+		};
+
+		j = nlohmann::json{
+			{"existing_ids", raw_ids}
+		};
+		return j;
+	};
+
+	int existing_from_json(nlohmann::json j) {
+		for (unsigned int raw_id : j["existing_ids"]) {
+			id::ID* current_id = id::create_id(static_cast<unsigned int>(raw_id), nullptr, nullptr, nullptr);
+		};
+		return 0;
+	};
+};
+
+#pragma endregion
+
+customer::Customer* customer::create_customer(std::string name, std::string phone_number, std::string email, unsigned int id )
 {
 	if (phone_number == "" && email == "") {
 		return nullptr;
 	};
-	customer::Customer* cus = new customer::Customer*(name, phone_number, email);
+	customer::Customer* cus = new customer::Customer;
+	cus->name = name;
+	cus->number = phone_number;
+	cus->email = email;
+
+	if (id == NULL) {
+		id = id::generate_unique_id();
+	};
+	cus->id = id;
+
 	return cus;
 }
 
-note::Note* create_note(std::string content, workorder::WorkOrder* work_order, customer::Customer* customer)
+note::Note* note::create_note(
+	std::string content, 
+	workorder::WorkOrder* work_order, 
+	customer::Customer* customer, 
+	unsigned int id,
+	std::string timestr
+)
 {
 	if (work_order == nullptr && customer == nullptr) {
 		return nullptr;
@@ -18,25 +103,47 @@ note::Note* create_note(std::string content, workorder::WorkOrder* work_order, c
 	time_t t = time(NULL);
 	struct tm* date;
 	date = gmtime(&t);
+	
+	if (timestr.empty()) {
+		timestr = asctime(date);
+	};
 
-	std::string timestr = asctime(date);
-	note::Note* note = new Note(timestr, content);
-	return note;
+	note::Note* n = new note::Note;
+	n->timestr = timestr;
+	n->content = content;
+
+	if (work_order != nullptr) {
+		work_order->notes.push_back(n);
+	};
+	if (customer != nullptr) {
+		customer->notes.push_back(n);
+	};
+
+	if (id == NULL) {
+		id = id::generate_unique_id();
+	};
+	n->id = id;
+	return n;
 }
 
-workorder::WorkOrder* create_work_order(customer::Customer* customer, std::string device, std::string description, float price)
+workorder::WorkOrder* create_work_order(customer::Customer* customer, std::string device, std::string description, float price, unsigned int id)
 {
 	if (price < 0) {
 		return nullptr;
 	};
-	workorder::WorkOrder* work_order = new WorkOrder(
-		customer,
-		device,
-		description,
-		price
-	);
+	workorder::WorkOrder* work_order = new workorder::WorkOrder;
+
+	work_order->customer = customer;
+	work_order->device = device;
+	work_order->description = description;
+	work_order->price = price;
+
+	if (id == NULL) {
+		id = id::generate_unique_id();
+	};
+	work_order->id = id;
 	return work_order;
-}
+};
 
 
 nlohmann::json note::to_json(note::Note* note) {
@@ -46,8 +153,14 @@ nlohmann::json note::to_json(note::Note* note) {
 };
 
 note::Note* note::from_json(nlohmann::json j) {
-	note::Note* n;
-	n->asctime = j["asctime"];
+	note::Note* n = note::create_note(
+		j["content"],
+		nullptr,
+		nullptr,
+		j["id"],
+		j["asctime"]
+	)
+	n->timestr = j["asctime"];
 	n->content = j["content"];
 	n->id = j["id"];
 	return n;
@@ -75,7 +188,9 @@ nlohmann::json workorder::to_json(workorder::WorkOrder* workorder) {
 };
 
 workorder::WorkOrder* workorder::from_json(nlohmann::json j) {
-	note::Note* n;
+	workorder::Workorder* n = workorder::create_work_order(
+	
+	);
 	n->asctime = j["asctime"];
 	n->content = j["content"];
 	return n;
--- ticketer/core.h
+++ ticketer/core.h
@@ -2,6 +2,7 @@
 #include <datetimeapi.h>
 #include <ctime>
 #include <chrono>
+#include <iostream>
 
 
 
@@ -17,26 +18,55 @@ nlohmann::json read_json(std::string filepath);
 
 
 
-
 namespace data {
 
-	std::vector<Note*> notes;
-	std::vector<WorkOrder*> workorders;
-	std::vector<Customer*> customers;
+	std::vector<note::Note*> notes;
+	std::vector<workorder::WorkOrder*> workorders;
+	std::vector<customer::Customer*> customers;
 
 };
 
 
+namespace id {
+
+	unsigned int generate_unique_id();
+
+	std::vector<id::ID*> existing_ids;
+
+	struct ID {
+		unsigned int raw_id;
+		workorder::WorkOrder* work_order = nullptr;
+		customer::Customer* customer = nullptr;
+		note::Note* note = nullptr;
+	};
+
+	id::ID* create_id(
+		unsigned int raw_id = NULL,
+		workorder::WorkOrder* work_order = nullptr,
+		customer::Customer* customer = nullptr,
+		note::Note* note = nullptr
+	);
+
+	id::ID* from_raw_id(unsigned int raw_id);
+	int existing_from_json(nlohmann::json j);
+	nlohmann::json existing_as_json();
+}
 
 
 namespace note {
 
 	struct Note {
-		std::string asctime;
+		std::string timestr;
 		std::string content;
 		unsigned int id;
 	};
-	Note* create_note(std::string content, WorkOrder* work_order = nullptr, Customer* customer = nullptr);
+	Note* create_note(
+		std::string content, 
+		workorder::WorkOrder* work_order = nullptr,
+		customer::Customer* customer = nullptr,
+		unsigned int id = NULL,
+		std::string timestr = NULL
+	);
 
 	nlohmann::json to_json(Note* note);
 	Note* from_json(nlohmann::json j);
@@ -48,10 +78,15 @@ namespace customer {
 		std::string name;
 		std::string number = "";
 		std::string email = "";
-		std::vector<Note*> notes = {};
+		std::vector<note::Note*> notes = {};
 		unsigned int id;
 	};
-	Customer* create_customer(std::string name, std::string phone_number, std::string email = "");
+	Customer* create_customer(
+		std::string name, 
+		std::string phone_number, 
+		std::string email = "", 
+		unsigned int id=NULL
+	);
 
 	nlohmann::json to_json(Customer* note);
 	Customer* from_json(nlohmann::json j);
@@ -60,15 +95,21 @@ namespace customer {
 namespace workorder {
 
 	struct WorkOrder {
-		Customer* customer;
+		customer::Customer* customer;
 		std::string device;
 		std::string description;
 		float price;
-		std::vector<Note*> notes = {};
+		std::vector<note::Note*> notes = {};
 		unsigned int id;
 
 	};
-	WorkOrder* create_work_order(Customer* customer, std::string device, std::string description, float price);
+	WorkOrder* create_work_order(
+		customer::Customer* c,
+		std::string device,
+		std::string description,
+		float price,
+		unsigned int id = NULL
+	);
 
 	nlohmann::json to_json(WorkOrder* workorder);
 	WorkOrder* from_json(nlohmann::json j);
